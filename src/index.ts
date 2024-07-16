35332dcc228427e91bf7f054ea6e1118068dbc5d
alexmenin <alexmenin_@hotmail.com>
2024-07-16 06:51:15 -0300

    Instalação do Sentry

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,17 @@ export * from './WAM'
 export type WASocket = ReturnType<typeof makeWASocket>
 export { makeWASocket }
 export default makeWASocket
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://16b890752571e314bc90deb029d014f8@o4507444383186944.ingest.us.sentry.io/4507610856882176",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
